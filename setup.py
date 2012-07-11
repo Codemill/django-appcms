80020165780a96322c287e26fc6816d673e92e0d
Ludvig Widman <ludvig.widman@codemill.se>
2012-07-11 13:31:49 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+version = __import__('appcms').__version__
+
+setup(
+    name = 'django-appcms',
+    version = version,
+    description = 'Templatetags for inserting django-cms placeholders in app templates.',
+    author = 'Ludvig Widman, CodeMill AB',
+    author_email = 'opensource@codemill.se',
+    url = 'http://github.com/codemill/django-appcms',
+    packages = find_packages(),
+    install_requires = [
+        'django-cms>=2.1',
+    ],
+)
