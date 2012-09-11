d4e41727de7e19b833596c5942be1d56ec9ff04b
Ludvig Widman <ludvig.widman@codemill.se>
2012-09-11 16:21:06 +0200

    Added language awareness to placeholder admin

--- appcms/admin.py
+++ appcms/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from appcms.models import *
 from cms.admin.placeholderadmin import PlaceholderAdmin
 
-admin.site.register(Placeholder, PlaceholderAdmin)
+class AppCmsPlaceholderAdmin(PlaceholderAdmin):
+
+    def placeholder_plugin_filter(self, request, queryset):
+        return queryset.filter(language=request.LANGUAGE_CODE)
+
+admin.site.register(Placeholder, AppCmsPlaceholderAdmin)
