e038b23f488239f33bd55b46e29d4d8d7b995326
Ludvig Widman <ludvig.widman@codemill.se>
2012-11-05 08:28:37 +0100

    Added cacheing

--- appcms/templatetags/appcms_tags.py
+++ appcms/templatetags/appcms_tags.py
@@ -2,6 +2,7 @@ from django import template
 from classytags.arguments import Argument
 from classytags.core import Tag, Options
 from django.template.defaultfilters import safe
+from django.core.cache import cache
 from ..models import Placeholder
 register = template.Library()
 
@@ -13,13 +14,26 @@ class RenderPlaceholder(Tag):
     )
 
     def render_tag(self, context, name, width):
+
+        def _get_placeholder(name, context, width):
+            placeholder, created = Placeholder.objects.get_or_create(name=name)
+            placeholder.placeholder.slot = name
+            return safe(placeholder.placeholder.render(context, width))
+
         request = context.get('request', None)
         if not request:
             return ''
         if not name:
             return ''
+        
+        if not request.user.is_staff:
+            cached = cache.get('placeholder-%s' % name)
+            if cached:
+                return cached
+            else:
+                resp = _get_placeholder(name, context, width) 
+                cache.set('placeholder-%s' % name, resp, 60)
+                return resp
 
-        placeholder, created = Placeholder.objects.get_or_create(name=name)
-        placeholder.placeholder.slot = name
-        return safe(placeholder.placeholder.render(context, width))
+        return _get_placeholder(name, context, width) 
 register.tag(RenderPlaceholder)
