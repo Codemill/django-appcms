4228a7d78acfdcd909f9d0a5f8573073e87bad61
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2014-01-02 07:26:47 +0100

    Fixed issue with admin

--- appcms/admin.py
+++ appcms/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from appcms.models import *
-from cms.admin.placeholderadmin import TranslatableAdmin, PlaceholderAdmin
+from cms.admin.placeholderadmin import PlaceholderAdmin
 
 
-class AppCmsPlaceholderAdmin(TranslatableAdmin, PlaceholderAdmin):
+class AppCmsPlaceholderAdmin(PlaceholderAdmin):
 
     def placeholder_plugin_filter(self, request, queryset):
         return queryset.filter(language=request.LANGUAGE_CODE)
