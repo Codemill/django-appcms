6c7dd0df2065cd0775bcec294cc7e9e33e7d361d
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2014-01-02 07:11:08 +0100

    Translatable admin

--- appcms/admin.py
+++ appcms/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from appcms.models import *
-from cms.admin.placeholderadmin import PlaceholderAdmin
+from cms.admin.placeholderadmin import TranslatableAdmin, PlaceholderAdmin
 
-class AppCmsPlaceholderAdmin(PlaceholderAdmin):
+
+class AppCmsPlaceholderAdmin(TranslatableAdmin, PlaceholderAdmin):
 
     def placeholder_plugin_filter(self, request, queryset):
         return queryset.filter(language=request.LANGUAGE_CODE)
